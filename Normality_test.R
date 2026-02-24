56cc2ea017400a64473f00622855d4a666b26be5
Sean Dempsey <sean@dhcp-206-87-129-117.ubcsecure.wireless.ubc.ca>
2026-02-24 12:54:45 -0800

    edited normality test and made figures

--- Normality_test.R
+++ Normality_test.R
@@ -1,5 +1,6 @@
 ### This script is to test for data normality
 library(tidyverse)
+library(cowplot)
 ### each person set their own working directory - setwd("/Users/sean/Desktop/Moss_analysis/ENVR400-Moss")
 kits_data <- read.csv("DATA COLLECTION - Cleaned Kits Values.csv", stringsAsFactors = TRUE) %>% 
   mutate(neighbourhood = "kits")
@@ -29,23 +30,42 @@ summarized_data <- all_data %>%
 write_csv(summarized_data, "combined_summarized_data.csv")
 
 
-#histograms
-ggplot(summarized_data, aes(x=mean_bryo_cover)) +
+##### Histograms!!!!!
+bryo_cover_histogram <- ggplot(summarized_data, aes(x=mean_bryo_cover)) +
   geom_histogram()+ #STRONG RIGHT SKEW
+  labs(x = "Mean Bryophyte Percent Cover",
+       y = "Count") +
   theme_classic()
-ggplot(summarized_data, aes(x = mean_lichen_cover)) +
+lichen_cover_histogram <- ggplot(summarized_data, aes(x = mean_lichen_cover)) +
   geom_histogram() + #SLIGHT RIGHT SKEW
+  labs(x = "Mean Lichen Percent Cover",
+       y = "Count") +
   theme_classic()
-ggplot(summarized_data, aes(x = mean_species_count)) +
+species_histogram <- ggplot(summarized_data, aes(x = mean_species_count)) +
   geom_histogram() + #NORMAL
+  labs(x = "Mean Species Count",
+       y = "Count") +
   theme_classic()
 
-### log transformed for bryo cover
-ggplot(summarized_data, aes(x=log(mean_bryo_cover +1))) +
+initial_histogram_plot <- plot_grid(bryo_cover_histogram, lichen_cover_histogram, species_histogram, ncol = 1)
+initial_histogram_plot
+ggsave("Figures/initial_histograms.png", plot = initial_histogram_plot, height = 8, width = 5)
+
+### Try to Log Transform Bryo Cover
+log_transformed_Bryo_histogram <- ggplot(summarized_data, aes(x=log(mean_bryo_cover +1))) +
   geom_histogram() + # wait but this won;t work with all the 0 values, so I added a plus one - need to make sure this is scientifically legit
 # So this doesn't work enough - too many 0 values
+  labs(x = "Log(Mean Bryophyte Cover +1)",
+       y = "Count") +
   theme_classic()
 
-ggplot(summarized_data, aes(x = (mean_lichen_cover)^(1/2))) +
+## Try to root transform lichen cover
+root_transformed_lichen_hist <- ggplot(summarized_data, aes(x = (mean_lichen_cover)^(1/2))) +
   geom_histogram() + #### Square root transformation works well on lichen!!!!
+  labs(x = "Srqt(Mean Lichen Cover)",
+       y = "Count") +
   theme_classic()
+
+final_histogram_plot <- plot_grid(log_transformed_Bryo_histogram, root_transformed_lichen_hist, species_histogram, ncol = 1)
+final_histogram_plot
+ggsave("Figures/final_histograms.png", plot = initial_histogram_plot, height = 8, width = 5)
